d4e6eb65b9705e86dbc1d2e658024d782d80120c
Marcin Raburski <rabursky@gmail.com>
2025-04-12 14:34:26 +0200

    logs: NetworkManager

--- backend/src/NetworkManager.ts
+++ backend/src/NetworkManager.ts
@@ -14,6 +14,7 @@ export class NetworkManager implements EventManager {
 	private lastMessageTimestamps: Map<string, number>
 	private readonly TIMEOUT_CHECK_INTERVAL = 5000 // 5 seconds
 	private readonly MAX_INACTIVE_TIME = 6000 // 6 seconds
+	private debug = true
 
 	constructor(io: Server) {
 		this.io = io
@@ -24,6 +25,8 @@ export class NetworkManager implements EventManager {
 		this.leftCallbacks = new Set()
 		this.lastMessageTimestamps = new Map()
 
+		if (this.debug) console.log('[NetworkManager] Initialized')
+
 		// Set up connection handler
 		this.io.on('connection', this.handleConnection.bind(this))
 
@@ -32,6 +35,7 @@ export class NetworkManager implements EventManager {
 	}
 
 	private checkInactiveClients() {
+		if (this.debug) console.log('[NetworkManager] Checking for inactive clients')
 		// const now = Date.now()
 		// for (const [clientId, lastMessageTime] of this.lastMessageTimestamps.entries()) {
 		// 	if (now - lastMessageTime > this.MAX_INACTIVE_TIME) {
@@ -49,14 +53,17 @@ export class NetworkManager implements EventManager {
 	}
 
 	private updateClientTimestamp(clientId: string) {
+		if (this.debug) console.log(`[NetworkManager] Updating timestamp for client: ${clientId}`)
 		this.lastMessageTimestamps.set(clientId, Date.now())
 	}
 
 	private removeClientFromGroup(clientId: string, group: string): void {
+		if (this.debug) console.log(`[NetworkManager] Removing client ${clientId} from group ${group}`)
 		const clients = this.groupClients.get(group)
 		if (clients) {
 			clients.delete(clientId)
 			if (clients.size === 0) {
+				if (this.debug) console.log(`[NetworkManager] Group ${group} is now empty, removing`)
 				this.groupClients.delete(group)
 			}
 		}
@@ -64,6 +71,7 @@ export class NetworkManager implements EventManager {
 	}
 
 	createNetworkClient(socket: Socket): EventClient {
+		if (this.debug) console.log(`[NetworkManager] Creating network client for socket: ${socket.id}`)
 		const self = this
 		return {
 			id: socket.id,
@@ -71,6 +79,7 @@ export class NetworkManager implements EventManager {
 				return self.clientGroups.get(socket.id) || DEFAULT_GROUP
 			},
 			setGroup(group: string) {
+				if (self.debug) console.log(`[NetworkManager] Setting group ${group} for client ${socket.id}`)
 				// Remove from previous group if exists
 				const previousGroup = self.clientGroups.get(socket.id)
 				if (previousGroup) {
@@ -85,8 +94,19 @@ export class NetworkManager implements EventManager {
 				}
 				clients.add(socket.id)
 				self.clientGroups.set(socket.id, group)
+				if (self.debug) console.log(`[NetworkManager] Client ${socket.id} added to group ${group}. Group size: ${clients.size}`)
 			},
 			emit: (to: Receiver, event: string, data: any, targetClientId?: string) => {
+				if (self.debug) {
+					console.log('[NetworkManager] Client emit:', {
+						from: socket.id,
+						to,
+						event,
+						targetClientId,
+						currentGroup: self.clientGroups.get(socket.id)
+					})
+				}
+				
 				// Update timestamp when client emits a message
 				self.updateClientTimestamp(socket.id)
 				
@@ -99,6 +119,7 @@ export class NetworkManager implements EventManager {
 						const group = self.clientGroups.get(socket.id)
 						if (group) {
 							const clients = self.groupClients.get(group)
+							if (self.debug) console.log(`[NetworkManager] Emitting to group ${group}, clients: ${Array.from(clients || [])}`)
 							clients?.forEach(clientId => {
 								self.io.to(clientId).emit(event, data)
 							})
@@ -110,6 +131,7 @@ export class NetworkManager implements EventManager {
 						const group = self.clientGroups.get(socket.id)
 						if (group) {
 							const clients = self.groupClients.get(group)
+							if (self.debug) console.log(`[NetworkManager] Emitting to group ${group} except sender ${socket.id}`)
 							clients?.forEach(clientId => {
 								if (clientId !== socket.id) {
 									self.io.to(clientId).emit(event, { ...data, sourcePlayerId: socket.id })
@@ -120,11 +142,13 @@ export class NetworkManager implements EventManager {
 					}
 					
 					case Receiver.All:
+						if (self.debug) console.log('[NetworkManager] Broadcasting to all clients')
 						self.io.emit(event, data)
 						break
 					
 					case Receiver.Client:
 						if (targetClientId) {
+							if (self.debug) console.log(`[NetworkManager] Emitting directly to client ${targetClientId}`)
 							self.io.to(targetClientId).emit(event, data)
 						}
 						break
@@ -134,17 +158,21 @@ export class NetworkManager implements EventManager {
 	}
 
 	private handleConnection(socket: Socket) {
+		if (this.debug) console.log(`[NetworkManager] New client connected: ${socket.id}`)
 		const client = this.createNetworkClient(socket)
 		
 		// Initialize timestamp for new connection
 		this.updateClientTimestamp(socket.id)
 
 		// Notify joined callbacks
+		if (this.debug) console.log(`[NetworkManager] Notifying ${this.joinedCallbacks.size} joined callbacks`)
 		this.joinedCallbacks.forEach(callback => callback(client))
 
 		// When a socket connects, set up all the event handlers for it
+		if (this.debug) console.log(`[NetworkManager] Setting up ${this.eventHandlers.size} event handlers for client ${socket.id}`)
 		this.eventHandlers.forEach((callbacks, event) => {
 			socket.on(event, (data: any) => {
+				if (this.debug) console.log(`[NetworkManager] Received event ${event} from client ${socket.id}`)
 				// Update timestamp when client sends any event
 				this.updateClientTimestamp(socket.id)
 				callbacks.forEach(callback => callback(data, client))
@@ -153,6 +181,7 @@ export class NetworkManager implements EventManager {
 
 		// Set up disconnect handler
 		socket.on('disconnect', () => {
+			if (this.debug) console.log(`[NetworkManager] Client disconnected: ${socket.id}`)
 			// Clean up group membership on disconnect
 			const group = this.clientGroups.get(socket.id)
 			if (group) {
@@ -163,6 +192,7 @@ export class NetworkManager implements EventManager {
 			this.lastMessageTimestamps.delete(socket.id)
 			
 			// Notify left callbacks
+			if (this.debug) console.log(`[NetworkManager] Notifying ${this.leftCallbacks.size} left callbacks`)
 			this.leftCallbacks.forEach(callback => callback(client))
 			
 			const handlers = this.eventHandlers.get('disconnect')
@@ -173,29 +203,44 @@ export class NetworkManager implements EventManager {
 	}
 
 	onJoined(callback: LifecycleCallback): void {
+		if (this.debug) console.log('[NetworkManager] Adding joined callback')
 		this.joinedCallbacks.add(callback)
 	}
 
 	onLeft(callback: LifecycleCallback): void {
+		if (this.debug) console.log('[NetworkManager] Adding left callback')
 		this.leftCallbacks.add(callback)
 	}
 
 	on<T>(event: string, callback: EventCallback<T>): void {
+		if (this.debug) console.log(`[NetworkManager] Registering handler for event: ${event}`)
 		// Add the callback to our handlers map
 		const handlers = this.eventHandlers.get(event) || []
 		handlers.push(callback as EventCallback)
 		this.eventHandlers.set(event, handlers)
 
 		// Also set up the handler for all existing sockets
+		if (this.debug) console.log(`[NetworkManager] Setting up handler for ${event} on ${this.io.sockets.sockets.size} existing sockets`)
 		this.io.sockets.sockets.forEach(socket => {
 			const client = this.createNetworkClient(socket)
 			socket.on(event, (data: T) => {
+				if (this.debug) console.log(`[NetworkManager] Handling event ${event} from existing client ${socket.id}`)
 				callback(data, client)
 			})
 		})
 	}
 
 	emit(to: Receiver, event: string, data: any, groupName?: string, originalClient?: EventClient): void {
+		if (this.debug) {
+			console.log('[NetworkManager] Global emit:', {
+				to,
+				event,
+				groupName,
+				hasOriginalClient: !!originalClient,
+				originalClientId: originalClient?.id
+			})
+		}
+
 		if (to === Receiver.Sender || to === Receiver.NoSenderGroup) {
 			if (!originalClient) {
 				throw new Error(`Cannot use ${to} with global emit. These receivers are only available for client-specific emissions.`)
@@ -207,11 +252,13 @@ export class NetworkManager implements EventManager {
 
 		switch (to) {
 			case Receiver.All:
+				if (this.debug) console.log('[NetworkManager] Broadcasting to all clients')
 				this.io.emit(event, data)
 				break
 			
 			case Receiver.Client:
 				if (groupName) { // groupName is used as targetClientId in this case
+					if (this.debug) console.log(`[NetworkManager] Emitting to specific client: ${groupName}`)
 					this.io.to(groupName).emit(event, data)
 				}
 				break
@@ -219,12 +266,14 @@ export class NetworkManager implements EventManager {
 			default:
 				if (groupName) {
 					const clients = this.groupClients.get(groupName)
+					if (this.debug) console.log(`[NetworkManager] Emitting to group ${groupName}, clients: ${Array.from(clients || [])}`)
 					if (clients) {
 						clients.forEach(clientId => {
 							this.io.to(clientId).emit(event, data)
 						})
 					}
 				} else {
+					if (this.debug) console.log('[NetworkManager] No group specified, broadcasting to all')
 					this.io.emit(event, data)
 				}
 		}
